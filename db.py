0fcb2f1f655f7abfcabbad1029065184978d75cf
kaistorecl <rosaleshernan1987@gmail.com>
2025-10-24 01:44:19 -0300

    Update comments for clarity in db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
 
-# Usa Postgres si defines DATABASE_URL. Si no, usa SQLite en /var/tmp (ruta escribible en Render)
+# Si no hay DATABASE_URL (Postgres), usa SQLite en /var/tmp (escribible en Render)
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////var/tmp/kaistore.db")
 
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
@@ -12,7 +12,6 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 class Base(DeclarativeBase):
     pass
 
-# Dependency para FastAPI (si la usas en routers)
 def get_db():
     db = SessionLocal()
     try:
